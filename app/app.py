ac3ed5a2d4a8cd3fdf09996bb00830084103e28f
Aigerim Zhaiymbet <s_zhaiymbet24@stud.hwr-berlin.de>
2025-05-21 20:40:48 +0200

    host, pass

--- app/app.py
+++ app/app.py
@@ -5,7 +5,8 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv() 
-cache = redis.Redis(host=os.getenv('REDIS_HOST'), port=6379,  password=os.getenv('REDIS_PASSWORD'))
+# cache = redis.Redis(host=os.getenv('REDIS_HOST'), port=6379,  password=os.getenv('REDIS_PASSWORD'))
+cache = redis.Redis(host="srv-captain--redis", port=6379, db=0, password='MyBIPMPassword')
 app = Flask(__name__)
 
 def get_hit_count():
