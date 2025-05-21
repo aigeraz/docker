927dd99f3bc850790de3b959c73c7caf4c6b4aef
Aigerim Zhaiymbet <s_zhaiymbet24@stud.hwr-berlin.de>
2025-05-21 11:18:06 +0200

    qwerty

--- app/app.py
+++ app/app.py
@@ -1,42 +1,28 @@
 import time
-import os
 import redis
 from flask import Flask, render_template
+import os
 from dotenv import load_dotenv
 
-# Load environment variables from .env file
-load_dotenv()
-
-# print("REDIS_HOST:", os.getenv("REDIS_HOST"))
-cache = redis.Redis(
-    host='srv-captain--redis',
-    port=6379,
-    password='MyBIPMPassword',
-    decode_responses=True
-)
-
-
+# load_dotenv() 
+cache = redis.Redis(host=os.getenv('REDIS_HOST'), port=6379,  password=os.getenv('REDIS_PASSWORD'))
 app = Flask(__name__)
 
 def get_hit_count():
     retries = 5
-    while retries > 0:
+    while True:
         try:
             return cache.incr('hits')
         except redis.exceptions.ConnectionError as exc:
-            retries -= 1  # decrement retries!
-            print(f"Redis connection failed: {type(exc).__name__} - {exc}. Retries left: {retries}")
+            if retries == 0:
+                raise exc
+            retries -= 1
             time.sleep(0.5)
-    raise ConnectionError("Could not connect to Redis after multiple retries.")
 
 @app.route('/')
 def hello():
-    try:
-        count = get_hit_count()
-    except Exception as e:
-        count = 'N/A'
-        print(f"Error getting hit count: {e}")
-    return render_template('hello.html', name="BIPM", count=count)
+    count = get_hit_count()
+    return render_template('hello.html', name= "BIPM", count = count)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=False)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80, debug=True)
